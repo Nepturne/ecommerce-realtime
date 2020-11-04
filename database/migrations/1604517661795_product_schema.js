5b73dd6435b0740f33897e22bc9f647e18c4b4e3
Nepturne <lucaspereira142010@gmail.com>
2020-11-04 16:59:18 -0300

    Migrations de Produtos /Cupons de Desconto

--- database/migrations/1604517661795_product_schema.js
+++ database/migrations/1604517661795_product_schema.js
@@ -13,7 +13,8 @@ class ProductSchema extends Schema {
       table.decimal('price', 12,2)
       table.timestamps()
 
-      table.foreign('image_id')
+      table
+      .foreign('image_id')
       .references('id')
       .inTable('images')
       .onDelete('cascade')
@@ -27,21 +28,42 @@ class ProductSchema extends Schema {
       table.integer('image_id').unsigned()
       table.integer('product_id').unsigned()
 
-      table.foreign('image_id')
+      table
+      .foreign('image_id')
       .references('id')
       .inTable('images')
       .onDelete('cascade')
 
-      table.foreign('product_id')
+      table
+      .foreign('product_id')
       .references('id')
       .inTable('products')
       .onDelete('cascade')
 
     })
 
+    this.create('category_product' , table => {
+      table.increments()
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+
+      table
+      .foreign('product_id')
+      .references('id')
+      .inTable('products')
+      .onDelete('cascade')
+
+      table
+      .foreign('category_id')
+      .references('id')
+      .inTable('categories')
+      .onDelete('cascade')
+    })
+
   }
 
   down () {
+    this.drop('category_product')
     this.drop('image_product')
     this.drop('products')
   }
