400aa8ed77771613ef76281c41dcf83498c05477
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2022-03-04 01:08:14 -0300

    image controller

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -5,6 +5,7 @@
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
 const Image = use('App/Models/Image')
+const { manage_single_upload } = use('App/Helpers')
 /**
  * Resourceful controller for interacting with images
  */
@@ -26,18 +27,6 @@ class ImageController {
   }
 
   /**
-   * Render a form to be used for creating a new image.
-   * GET images/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
    * Create/save a new image.
    * POST images
    *
@@ -46,6 +35,36 @@ class ImageController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+    try {
+      // captura uma image ou mais no request
+      const fileJar = request.file('images', {
+        types: ['image'],
+        size: '2mb'
+      })
+
+      // retorno pro usuário
+      let images = []
+      //caso seja um único arquivo - manage_single_upload
+      //caso sejam vários arquivos - manage_multiple_uploads
+      if(!fileJar.files){
+        const file = await manage_single_upload(fileJar)
+        if(file.moved()){
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+
+
+          images.push(image)
+
+          return response.status(201).send({success: images, errors: {} })
+        }
+      }
+    } catch (error) {
+      
+    }
   }
 
   /**
