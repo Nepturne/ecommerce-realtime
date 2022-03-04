2d1ae351153474bb456ce8be8d28551342ba8cb1
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2022-03-04 00:57:44 -0300

    Images

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -4,6 +4,7 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Image = use('App/Models/Image')
 /**
  * Resourceful controller for interacting with images
  */
@@ -17,7 +18,11 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, pagination }) {
+    const images = await Image.query()
+    .orderBy('id', 'DESC')
+    .paginate(pagination.page,pagination.limit)
+    return response.send(images)
   }
 
   /**
--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -29,7 +29,60 @@ const str_random = async (length = 40) => {
     return string
 }
 
+/**
+ * Move a given file for the spcified path, if none is specified, move file to 'public/uploads' path.
+ * @param {FileJar} file
+ * @param {string} path
+ */
+ const manage_single_upload = async (file, path = null) => {
+    path = path ? path : Helpers.publicPath('uploads')
+    // gera um nome aleatório
+    const random_name = await str_random(30)
+    let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+    // renomeia o arquivo e move para a pasta public/uploads
+    await file.move(path, {
+        name: filename
+    })
+
+    return file
+}
+
+/**
+ * Move a given file list for the spcified path, if none is specified, move files to 'public/uploads' path.
+ * @param {Filejar} fileJar
+ * @param {string} path
+ * @return { object }
+ */
+ const manage_multiple_uploads = async (fileJar, path = null) => {
+    path = path ? path : Helpers.publicPath('uploads')
+    let successes = []
+    let errors = []
+
+    await Promise.all(
+        fileJar.files.map(async file => {
+            let random_name = await str_random(30)
+            let filename = `${new Date().getTime()}-${random_name}.${
+                file.subtype
+            }`
+            await file.move(path, {
+                name: filename
+            })
+
+            if (file.moved()) {
+                successes.push(file)
+            } else {
+                errors.push(file.error())
+            }
+        })
+    )
+    return { successes, errors }
+}
+
+
 
 module.exports = {
-    str_random
+    str_random,
+    manage_single_upload,
+    manage_multiple_uploads
 }
