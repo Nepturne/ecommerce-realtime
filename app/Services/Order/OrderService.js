dfa9fdfd636d0ddf28df7907d0f7bf76905bcfb6
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2022-03-04 13:20:20 -0300

    Order Service Methods

---
+++ app/Services/Order/OrderService.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const { TIMESTAMP } = require("mysql/lib/protocol/constants/types")
+
+class OrderService {
+    constructor(model, trx=false){
+        this.model = model
+        this.trx = trx
+    }
+
+
+    async syncItems(items){
+        if(!Array.isArray(items)){
+            return false
+        }
+        await this.model.items().delete(this.trx)
+        await this.model.items().createMany(items, this.trx)
+    }
+
+    async updateItems(items){
+        let currentItems = await this.model
+            .items()
+            .whereIn('id', items.map( item => item.id))
+            .fetch()
+            // deleta os itens que o user não quer mais
+            await this.model
+                .items()
+                .whereNotIn('id', items.map(item => item.id))
+                .delete(this.trx)
+
+            // Atualiza os valores e quantidades 
+            await Promise.all(currentItems.rows.map(async item =>{
+                item.fill(items.find(n => n.id === item.id))
+                await item.save(this.trx)
+            }))
+    }
+
+    
+
+} 
+
+module.exports = OrderService
+
