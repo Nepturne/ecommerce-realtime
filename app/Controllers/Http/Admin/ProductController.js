4e47597743dfa17805c5049f9a535a1d221215d7
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2022-03-08 18:50:10 -0300

    Product Transformer

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -4,6 +4,7 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 const Product = use('App/Models/Product')
+const Transformer = use('App/Transformers/Admin/ProductTransformer')
 /**
  * Resourceful controller for interacting with products
  */
@@ -17,7 +18,7 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, pagination }) {
+  async index ({ request, response, pagination , transform}) {
     const name = request.input('name')
     const query = Product.query()
 
@@ -25,7 +26,8 @@ class ProductController {
       query.where('name', 'LIKE', `%${name}%`)
     }
 
-    const products = await query.paginate(pagination.page, pagination.limit)
+    var products = await query.paginate(pagination.page, pagination.limit)
+    products = await transform.paginate(products, Transformer)
     return response.send(products)
   }
 
@@ -38,11 +40,12 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request, response, transform }) {
     
     try {
       const { name, description, price , image_id } = request.all()
-      const product = await Product.create({ name ,description , price , image_id })
+      var product = await Product.create({ name ,description , price , image_id })
+      product = await transform.item(product, Transformer)
       return response.status(201).send(product)
     } catch (error) {
       response.status(400).send({ message: 'Não foi possível criar o produto neste momento!' })
@@ -59,8 +62,9 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params: { id }, request, response }) {
-    const product = await Product.findOrFail(id)
+  async show ({ params: { id }, request, response, transform }) {
+    var product = await Product.findOrFail(id)
+    product = await transform.item(product, Transformer)
     return response.send(product)
   }
 
@@ -73,12 +77,13 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params: { id }, request, response }) {
-    const product = await Product.findOrFail(id)
+  async update ({ params: { id }, request, response, transform }) {
+    var product = await Product.findOrFail(id)
     try {
       const { name , description , price , image_id } = request.all()
       product.merge({ name, description , price , image_id })
       await product.save()
+      product = await transform.item(product, Transformer)
       return response.send(product)
     } catch (error) {
       return response
